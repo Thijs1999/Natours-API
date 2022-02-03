a2cd6b066e2bbae5018a38a8c235385945277071
Thijs Looijen <93253559+ThijsSoleoMedia@users.noreply.github.com>
2022-02-03 08:15:47 +0100

    Changes

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello there', app: 'Natours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'succes', results: tours.length, data: { tours } });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint....');
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'succes', data: { tour: newTour } });
+    }
+  );
 });
 
 const port = 3000;
