83ce96547eca8eaddc7189fda45e27774fc80be7
ThijsSoleoMedia <thijs@soleomedia.com>
2022-02-09 13:58:56 +0100

    Code refactor and added users to api

--- app.js
+++ app.js
@@ -1,34 +1,22 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const port = 3000;
 
 const app = express();
+
+app.use(morgan('dev'));
 app.use(express.json());
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
 
-app.get('/api/v1/tours', (req, res) => {
-  res
-    .status(200)
-    .json({ status: 'succes', results: tours.length, data: { tours } });
-});
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({ status: 'succes', data: { tour: newTour } });
-    }
-  );
-});
-
-const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
