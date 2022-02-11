7dcdf85f4a02c71fbf5a8c3453bea82141e117b5
ThijsSoleoMedia <thijs@soleomedia.com>
2022-02-11 13:05:52 +0100

    File & Code refactor

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const port = 3000;
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-})
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,73 +1,80 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-  );
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+
+  next();
+};
+
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour ID is: ${val}`);
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  next();
+};
 
 exports.getAllTours = (req, res) => {
-    res
-      .status(200)
-      .json({ status: 'succes', requestedAt: req.requestTime, results: tours.length, data: { tours } });
-  };
-  
+  res.status(200).json({
+    status: 'succes',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: { tours },
+  });
+};
+
 exports.getTour = (req, res) => {
-    const newId = req.params.id * 1;
-    const tour = tours.find(el => el.id === newId);
+  const newId = req.params.id * 1;
+  const tour = tours.find((el) => el.id === newId);
 
-    if(!tour) {
-        return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'succes',
-        data: {
-        tour
-        }
-    })
-};
-  
-exports.createTour =  (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-
-    tours.push(newTour);
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        (err) => {
-        res.status(201).json({ status: 'succes', data: { tour: newTour } });
-    }
-)};
-  
-exports.updateTour =  (req, res) => {  
-    if(req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-        tour: '<updated tour here>'
-        }
-    })
+  res.status(200).json({
+    status: 'succes',
+    data: {
+      tour,
+    },
+  });
 };
 
-exports.deleteTour = (req, res) => {  
-    if(req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid ID'
-        });
-    }
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
 
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
-}
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'succes', data: { tour: newTour } });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<updated tour here>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
