f60255ec7cbe264e602eccc2fa6602eab0944cc2
Thijs Looijen <93253559+ThijsSoleoMedia@users.noreply.github.com>
2022-02-21 21:09:40 +0100

    Eslint settup

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+
 const morgan = require('morgan');
+
 const tourRouter = require('./routes/tourRoutes');
+
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
